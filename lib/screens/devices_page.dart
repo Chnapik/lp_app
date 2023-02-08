722fe7ceb02176afaacab3ae1314cd97c66623ee
Derrick <dmwilliamson@gmail.com>
2023-02-08 10:30:03 -0600

    styled pages text widgets to fontSize of 40.0, removed unused code on devices page and unused imports
    Relates: #19

--- lib/screens/devices_page.dart
+++ lib/screens/devices_page.dart
@@ -1,64 +1,16 @@
 import 'package:flutter/material.dart';
 
-import 'ble1_page.dart';
-import 'home_page.dart';
-
-class MainRoute extends StatefulWidget {
-  const MainRoute({super.key});
-
-  @override
-  State<MainRoute> createState() => _MainRouteState();
-}
-
-class _MainRouteState extends State<MainRoute> {
-  int _selectedPageIndex = 1;
-
-  void _onPageChanged(int index) => setState(() => _selectedPageIndex = index);
-
-  final List<Widget> screens = [
-    const DevicesPage(),
-    const Ble1Page(),
-    const HomePage(),
-  ];
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: screens[_selectedPageIndex],
-      bottomNavigationBar: BottomNavigationBar(
-        type: BottomNavigationBarType.fixed,
-        currentIndex: _selectedPageIndex,
-        items: const [
-          BottomNavigationBarItem(
-            icon: Icon(Icons.phone_android),
-            label: 'Devices',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.battery_charging_full),
-            label: 'Battery',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            label: 'Home',
-          ),
-        ],
-        backgroundColor: Colors.blueGrey,
-        selectedItemColor: Colors.orangeAccent,
-        unselectedItemColor: Colors.white,
-        onTap: _onPageChanged,
-      ),
-    );
-  }
-}
-
 class DevicesPage extends StatelessWidget {
   const DevicesPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: const Center(
-        child: Text('Devices Page'),
+    return const Scaffold(
+      body: Center(
+        child: Text(
+          'Devices Page',
+          style: TextStyle(fontSize: 40.0),
+        ),
       ),
     );
   }
