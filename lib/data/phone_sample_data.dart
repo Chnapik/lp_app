a6caaa06faa41ec3c074a905c63c0ad90d586972
Derrick <dmwilliamson@gmail.com>
2023-02-09 17:20:46 -0600

    created numberOfCharges list and chargeLevel list and initialized with elements
    Relates: #12

--- lib/data/phone_sample_data.dart
+++ lib/data/phone_sample_data.dart
@@ -1,1 +1,4 @@
-class PhoneData {}
+class PhoneData {
+  final List<int> numberOfCharges = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  final List<int> chargeLevel = [100, 90, 80, 70, 60, 50, 40, 30, 20, 10];
+}
