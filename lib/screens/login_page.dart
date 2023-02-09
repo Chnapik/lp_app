cfc378775765ad889678be06e4c0327f47545e22
Derrick <dmwilliamson@gmail.com>
2023-02-09 16:12:18 -0600

    added singlechildscrollview to deal with screen overflow when phone keyboard is engaged
    Relates: #13

--- lib/screens/login_page.dart
+++ lib/screens/login_page.dart
@@ -10,50 +10,56 @@ class LoginPage extends StatelessWidget {
     return Scaffold(
       body: SafeArea(
         child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              const SizedBox(
-                height: 50.0,
-              ),
-              const Text(
-                'Logo',
-                style: TextStyle(
-                  fontSize: 40.0,
-                  fontWeight: FontWeight.w500,
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const SizedBox(
+                  height: 50.0,
                 ),
-              ),
-              const SizedBox(
-                height: 50.0,
-              ),
-              TextFormField(
-                decoration: const InputDecoration(
-                  hintText: 'Username',
-                  border: OutlineInputBorder(),
+                const Text(
+                  'Logo',
+                  style: TextStyle(
+                    fontSize: 40.0,
+                    fontWeight: FontWeight.w500,
+                  ),
                 ),
-              ),
-              const SizedBox(
-                height: 20.0,
-              ),
-              TextFormField(
-                decoration: const InputDecoration(
-                  hintText: 'Password',
-                  border: OutlineInputBorder(),
+                const Icon(
+                  Icons.battery_charging_full_rounded,
+                  size: 80.0,
                 ),
-              ),
-              const SizedBox(
-                height: 50.0,
-              ),
-              Builder(builder: (context) {
-                return ElevatedButton(
-                  onPressed: () {
-                    Navigator.of(context).push(MaterialPageRoute(
-                        builder: (context) => const MainRoute()));
-                  },
-                  child: const Text('Login'),
-                );
-              }),
-            ],
+                const SizedBox(
+                  height: 50.0,
+                ),
+                TextFormField(
+                  decoration: const InputDecoration(
+                    hintText: 'Username',
+                    border: OutlineInputBorder(),
+                  ),
+                ),
+                const SizedBox(
+                  height: 20.0,
+                ),
+                TextFormField(
+                  decoration: const InputDecoration(
+                    hintText: 'Password',
+                    border: OutlineInputBorder(),
+                  ),
+                ),
+                const SizedBox(
+                  height: 50.0,
+                ),
+                Builder(builder: (context) {
+                  return ElevatedButton(
+                    onPressed: () {
+                      Navigator.of(context).push(MaterialPageRoute(
+                          builder: (context) => const MainRoute()));
+                    },
+                    child: const Text('Login'),
+                  );
+                }),
+              ],
+            ),
           ),
         ),
       ),
