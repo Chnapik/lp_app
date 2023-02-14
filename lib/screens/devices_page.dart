998b01aae82824f39e4620cd0ba5fe74b33d437e
Derrick <dmwilliamson@gmail.com>
2023-02-14 09:10:40 -0600

    added Inkwells for BLE1, BLE2, and Add Device
    Relates: #37

--- lib/screens/devices_page.dart
+++ lib/screens/devices_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:lp_app/screens/app_bar.dart';
+import 'package:lp_app/screens/battery_page.dart';
 
 class DevicesPage extends StatelessWidget {
   const DevicesPage({Key? key}) : super(key: key);
@@ -11,12 +12,59 @@ class DevicesPage extends StatelessWidget {
         appBarTitle: 'Devices',
         showLeading: false,
       ),
-      body: const Center(
-        child: Text(
-          'Devices Page',
-          style: TextStyle(fontSize: 40.0),
-        ),
+      body: Center(
+        child: Builder(builder: (context) {
+          return Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              const Text(
+                'Devices',
+                style: TextStyle(fontSize: 50.0, fontWeight: FontWeight.bold),
+              ),
+              InkWell(
+                child: Container(
+                  child: const Text(
+                    'BLE1',
+                    style: TextStyle(fontSize: 30.0),
+                  ),
+                ),
+                onTap: () {
+                  Navigator.of(context).push(MaterialPageRoute(
+                      builder: (context) => const BatteryPage()));
+                },
+              ),
+              InkWell(
+                child: Container(
+                  child: const Text(
+                    'BLE2',
+                    style: TextStyle(fontSize: 30.0),
+                  ),
+                ),
+                onTap: () {
+                  Navigator.of(context).push(MaterialPageRoute(
+                      builder: (context) => const BatteryPage()));
+                },
+              ),
+              InkWell(
+                child: Container(
+                  child: const Text(
+                    'Add Device',
+                    style: TextStyle(fontSize: 30.0),
+                  ),
+                ),
+                onTap: () {},
+              ),
+            ],
+          );
+        }),
       ),
+
+      // body: const Center(
+      //   child: Text(
+      //     'Devices Page',
+      //     style: TextStyle(fontSize: 40.0),
+      //   ),
+      // ),
     );
   }
 }
