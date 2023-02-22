7f5f7c82a3c7075503ff59fa09924b0ac7c621ab
Derrick <dmwilliamson@gmail.com>
2023-02-22 10:58:08 -0600

    updates to listview.separated
    Relates: #51

--- lib/screens/devices_page.dart
+++ lib/screens/devices_page.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:lp_app/screens/battery_health_page.dart';
 import 'package:lp_app/screens/ble1_page.dart';
 import 'package:lp_app/screens/ble2_page.dart';
 
@@ -9,10 +8,13 @@ class DevicesPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final List<String> entries = <String>['BLE1', 'BLE2'];
-    final List<int> colorCodes = <int>[600, 500, 100];
+    // final List<int> colorCodes = <int>[600, 500, 100];
 
-    return SafeArea(
-      child: ListView.separated(
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Devices'),
+      ),
+      body: ListView.separated(
         padding: const EdgeInsets.all(8),
         itemCount: entries.length,
         itemBuilder: (BuildContext context, int index) {
@@ -24,68 +26,24 @@ class DevicesPage extends StatelessWidget {
               } else if (index == 1) {
                 Navigator.of(context).push(
                     MaterialPageRoute(builder: (context) => const Ble2Page()));
-              } else if (index == 2) {
-                Navigator.of(context).push(MaterialPageRoute(
-                    builder: (context) => const BatteryHealthPage()));
               }
             },
             child: Container(
               height: 50,
-              color: Colors.amber[colorCodes[index]],
-              child: Center(child: Text(entries[index])),
+              color: Colors.blueGrey,
+              // color: Colors.amber[colorCodes[index]],
+              child: Center(
+                  child: Text(
+                entries[index],
+                style: const TextStyle(color: Colors.white),
+              )),
             ),
           );
         },
-        separatorBuilder: (BuildContext context, int index) => const Divider(),
+        separatorBuilder: (BuildContext context, int index) => const Divider(
+          height: 30.0,
+        ),
       ),
     );
   }
 }
-
-
-
-// Scaffold(
-//       appBar: AppBar(
-//         title: const Text('Devices'),
-//       ),
-//       body: Center(
-//         child: Builder(builder: (context) {
-//           return Column(
-//             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-//             children: [
-//               InkWell(
-//                 child: Container(
-//                   child: const Text(
-//                     'BLE1',
-//                     style: TextStyle(fontSize: 30.0),
-//                   ),
-//                 ),
-//                 onTap: () {
-//                   Navigator.of(context).push(MaterialPageRoute(
-//                       builder: (context) => const Ble1Page()));
-//                 },
-//               ),
-//               InkWell(
-//                 child: Container(
-//                   child: const Text(
-//                     'BLE2',
-//                     style: TextStyle(fontSize: 30.0),
-//                   ),
-//                 ),
-//                 onTap: () {
-//                   Navigator.of(context).push(MaterialPageRoute(
-//                       builder: (context) => const Ble2Page()));
-//                 },
-//               ),
-//               InkWell(
-//                 child: const Text(
-//                   'Add Device',
-//                   style: TextStyle(fontSize: 30.0),
-//                 ),
-//                 onTap: () {},
-//               ),
-//             ],
-//           );
-//         }),
-//       ),
-//     );
