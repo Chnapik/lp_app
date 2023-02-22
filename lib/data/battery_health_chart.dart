629adee720ee9b2abdee5a68a42106d7eaf44b11
Derrick <dmwilliamson@gmail.com>
2023-02-22 17:52:17 -0600

    created battery health chart file and remove old phone sample data file
    Relates: #56

--- lib/data/battery_health_chart.dart
+++ lib/data/battery_health_chart.dart
@@ -1,9 +1,3 @@
-void main() {
-  final ble1 = PhoneData();
-  print(ble1.charges);
-  print(ble1.batteryLevel);
-}
-
 class PhoneData {
   final List<int> numberOfCharges = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
   final List<int> chargeLevel = [100, 90, 80, 70, 60, 50, 40, 30, 20, 10];
