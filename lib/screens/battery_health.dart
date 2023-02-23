c8bee88472eb4ec8d9ae92b0c156f5e5ec54a235
Derrick <dmwilliamson@gmail.com>
2023-02-22 19:42:01 -0600

    additional updates
    Relates: 56

--- lib/screens/battery_health.dart
+++ lib/screens/battery_health.dart
@@ -10,6 +10,7 @@ class ChartData {
 class ChargeLevelChart extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final _currentIndex = 0;
     final List<int> chargeLevels = [80, 60, 50, 30, 90, 75, 40, 65, 85];
 
     final List<ChartData> data = List.generate(
@@ -21,54 +22,24 @@ class ChargeLevelChart extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Battery Health'),
       ),
-      body: SafeArea(
-        child: Center(
-          child: SfCartesianChart(
-            title: ChartTitle(text: 'Charge Level History'),
-            primaryXAxis: NumericAxis(),
-            primaryYAxis: NumericAxis(),
-            series: <LineSeries<ChartData, int>>[
-              LineSeries<ChartData, int>(
-                dataSource: data,
-                xValueMapper: (ChartData data, _) => data.x,
-                yValueMapper: (ChartData data, _) => data.y,
-              ),
-            ],
+      body: Stack(
+        children: [
+          Positioned(
+            child: SfCartesianChart(
+              title: ChartTitle(text: 'Charge Level History'),
+              primaryXAxis: NumericAxis(),
+              primaryYAxis: NumericAxis(),
+              series: <LineSeries<ChartData, int>>[
+                LineSeries<ChartData, int>(
+                  dataSource: data,
+                  xValueMapper: (ChartData data, _) => data.x,
+                  yValueMapper: (ChartData data, _) => data.y,
+                ),
+              ],
+            ),
           ),
-        ),
+        ],
       ),
     );
   }
 }
-
-
-// import 'package:flutter/material.dart';
-
-// class BatteryHealthPage extends StatefulWidget {
-//   const BatteryHealthPage({Key? key}) : super(key: key);
-
-//   @override
-//   State<BatteryHealthPage> createState() => _BatteryHealthPageState();
-// }
-
-// class _BatteryHealthPageState extends State<BatteryHealthPage> {
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(
-//         title: const Text('Battery'),
-//       ),
-//       body: const Center(
-//         child: Text(
-//           'Battery Health Page',
-//           style: TextStyle(fontSize: 40.0),
-//         ),
-//       ),
-//     );
-//   }
-// }
-
-// class _BatteryHealthData {
-//   final List<int> _currentCharge = [100, 90, 80, 70, 60, 50, 40, 30, 20, 10];
-//   final List<int> _numberOfCharges = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-// }
