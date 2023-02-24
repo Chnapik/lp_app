8107e558d65b0fe30c132f1ac5db314dc2ee4c55
Derrick <dmwilliamson@gmail.com>
2023-02-23 19:18:24 -0600

    redesign of devices screen

--- lib/screens/devices.dart
+++ lib/screens/devices.dart
@@ -8,60 +8,148 @@ class DevicesPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final List<String> entries = <String>[
-      'BLE1',
-      'BLE2',
-      'Add Device',
-    ];
-    // final List<int> colorCodes = <int>[600, 500, 100];
-
     return Scaffold(
       appBar: AppBar(
         title: const Text('Devices'),
       ),
-      body: Container(
-        child: Card(
-          margin: const EdgeInsets.all(8.0),
-          color: const Color.fromARGB(255, 214, 211, 211),
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(15.0),
-          ),
-          child: ListView.separated(
-            padding: const EdgeInsets.all(25.0),
-            itemCount: entries.length,
-            itemBuilder: (BuildContext context, int index) {
-              return GestureDetector(
-                onTap: () {
-                  if (index == 0) {
-                    Navigator.of(context).push(MaterialPageRoute(
-                        builder: (context) => const Ble1Page()));
-                  } else if (index == 1) {
-                    Navigator.of(context).push(MaterialPageRoute(
-                        builder: (context) => const Ble2Page()));
-                  } else if (index == 2) {
-                    Navigator.of(context).push(MaterialPageRoute(
-                        builder: (context) => const AddDevice()));
-                  }
-                },
-                child: Container(
-                  height: 50,
+      body: ListView(
+        children: [
+          Padding(
+            padding: EdgeInsets.all(8.0),
+            child: GestureDetector(
+              onTap: () {
+                Navigator.of(context).push(
+                    MaterialPageRoute(builder: (context) => const Ble1Page()));
+              },
+              child: Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(15.0),
                   color: Colors.blueGrey,
-                  // color: Colors.amber[colorCodes[index]],
-                  child: Center(
-                      child: Text(
-                    entries[index],
-                    style: const TextStyle(color: Colors.white, fontSize: 18.0),
-                  )),
                 ),
-              );
-            },
-            separatorBuilder: (BuildContext context, int index) =>
-                const Divider(
-              height: 30.0,
+                height: 100.0,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Row(
+                      children: const [
+                        Icon(Icons.bluetooth, size: 40.0, color: Colors.white),
+                        Text(
+                          'BLE1',
+                          style: TextStyle(fontSize: 40.0, color: Colors.white),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
             ),
           ),
-        ),
+          Padding(
+            padding: EdgeInsets.all(8.0),
+            child: GestureDetector(
+              onTap: () {
+                Navigator.of(context).push(
+                    MaterialPageRoute(builder: (context) => const Ble2Page()));
+              },
+              child: Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(15.0),
+                  color: Colors.blueGrey,
+                ),
+                height: 100.0,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Row(
+                      children: const [
+                        Icon(Icons.bluetooth, size: 40.0, color: Colors.white),
+                        Text(
+                          'BLE2',
+                          style: TextStyle(fontSize: 40.0, color: Colors.white),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.all(8.0),
+            child: GestureDetector(
+              onTap: () {
+                Navigator.of(context).push(
+                    MaterialPageRoute(builder: (context) => const AddDevice()));
+              },
+              child: Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(15.0),
+                  color: Colors.blueGrey,
+                ),
+                height: 100.0,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Row(
+                      children: const [
+                        Icon(Icons.bluetooth, size: 40.0, color: Colors.white),
+                        Text(
+                          'Add Device',
+                          style: TextStyle(fontSize: 40.0, color: Colors.white),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
+
+      // body: Container(
+      //   child: Card(
+      //     margin: const EdgeInsets.all(8.0),
+      //     color: const Color.fromARGB(255, 214, 211, 211),
+      //     shape: RoundedRectangleBorder(
+      //       borderRadius: BorderRadius.circular(15.0),
+      //     ),
+      //     child: ListView.separated(
+      //       padding: const EdgeInsets.all(25.0),
+      //       itemCount: entries.length,
+      //       itemBuilder: (BuildContext context, int index) {
+      //         return GestureDetector(
+      //           onTap: () {
+      //             if (index == 0) {
+      //               Navigator.of(context).push(MaterialPageRoute(
+      //                   builder: (context) => const Ble1Page()));
+      //             } else if (index == 1) {
+      //               Navigator.of(context).push(MaterialPageRoute(
+      //                   builder: (context) => const Ble2Page()));
+      //             } else if (index == 2) {
+      //               Navigator.of(context).push(MaterialPageRoute(
+      //                   builder: (context) => const AddDevice()));
+      //             }
+      //           },
+      //           child: Container(
+      //             height: 50,
+      //             color: Colors.blueGrey,
+      //             // color: Colors.amber[colorCodes[index]],
+      //             child: Center(
+      //                 child: Text(
+      //               entries[index],
+      //               style: const TextStyle(color: Colors.white, fontSize: 18.0),
+      //             )),
+      //           ),
+      //         );
+      //       },
+      //       separatorBuilder: (BuildContext context, int index) =>
+      //           const Divider(
+      //         height: 30.0,
+      //       ),
+      //     ),
+      //   ),
+      // ),
     );
   }
 }
