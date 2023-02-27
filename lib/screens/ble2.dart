ce763a3046007901fa252ea573aba567dcfd748c
Derrick <dmwilliamson@gmail.com>
2023-02-27 16:50:49 -0600

    build out of settings screen with simple listview

--- lib/screens/ble2.dart
+++ lib/screens/ble2.dart
@@ -118,12 +118,6 @@ class Ble2Page extends StatelessWidget {
           ),
         ],
       ),
-      // body: const Center(
-      //   child: Text(
-      //     'BLE1 Page',
-      //     style: TextStyle(fontSize: 40.0),
-      //   ),
-      // ),
     );
   }
 }
@@ -133,30 +127,3 @@ class ChartData {
   final int x;
   final int y;
 }
-
-
-// import 'package:flutter/material.dart';
-
-// class Ble2Page extends StatelessWidget {
-//   const Ble2Page({super.key});
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(
-//         title: const Text('BLE2'),
-//         automaticallyImplyLeading: true,
-//         leading: IconButton(
-//           icon: const Icon(Icons.arrow_back),
-//           onPressed: () => Navigator.pop(context),
-//         ),
-//       ),
-//       body: const Center(
-//         child: Text(
-//           'BLE2 Page',
-//           style: TextStyle(fontSize: 40.0),
-//         ),
-//       ),
-//     );
-//   }
-// }
